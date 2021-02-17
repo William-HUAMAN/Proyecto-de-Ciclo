30218b2ae09e3670e690036a30d6c34f158eb7f3
Will-HH <whuaman@unitru.edu.pe>
2021-02-17 18:50:41 -0500

    Definiendo pantallas

--- Médico_App/main.py
+++ Médico_App/main.py
@@ -2,13 +2,19 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+class Informacion(Screen):
+    pass
+
 class Login(Screen):
+    pass
+
+class Pacientes(Screen):
     pass
 
 class MedicoApp(MDApp):
     def build(self):
         self.title='Medic-App'
-        self.theme.cls.primary_palette='Blue'
+        self.theme_cls.primary_palette='Blue'
         return Builder.load_file('main.kv')
 
 
